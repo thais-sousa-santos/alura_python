75b8156ff77071328415df054326d8dc1139c0c7
Thais Santos <thaissousasantos17@gmail.com>
2021-04-19 19:20:49 -0300

    Curso python, commit de atribuição de número randomico no jogo, 3 dificuldades de jogo "fácil, médio e difícil" e importação da biblioteca "random".

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,9 +1,23 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
-total_de_tentativas = 3
+numero_secreto = random.randrange(1,101)
+total_de_tentativas = 0
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina um nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
