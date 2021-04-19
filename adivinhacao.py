c4dc1c753592670e880c562b76b48b374f549cea
Thais Santos <thaissousasantos17@gmail.com>
2021-04-19 14:36:08 -0300

    Curso python, quarto commit de teste. -mudança na validação das rodadas, limitação de números válidos e indentação melhor de código.

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,13 +4,16 @@ print("*********************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while (rodada <= total_de_tentativas):
-    print("Tentativa", rodada, "de", total_de_tentativas)
-    chute_str = input("Digite o seu número: ")
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -18,11 +21,11 @@ while (rodada <= total_de_tentativas):
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif (menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
-    rodada = rodada + 1
 
-print("Fim do jogo")
+print("Fim do jogo!")
