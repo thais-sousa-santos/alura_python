df686f839f3b253c20b1222acfe0f79fcb0e636e
Thais Santos <thaissousasantos17@gmail.com>
2021-05-03 16:10:36 -0300

    Curso python, commit da inicialização de desenvolvimento do jogo forca, onde já podemos identificar as letras e comparar com a palavra secreta que escolhemos para o usuário adivinhar.

--- forca.py
+++ forca.py
@@ -2,8 +2,25 @@
 
 def jogar():
     print("*********************************")
-    print("*** Bem vindo ao jogo de Forca! ***")
+    print("Bem vindo ao jogo de Forca!")
     print("*********************************")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra?")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("Jogando..")
 
     print("Fim do jogo!")
 
